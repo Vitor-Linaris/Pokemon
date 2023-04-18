e678e775507f986c4a547167525e01da051957eb
Vitor-Linaris <vitor.linaris@gmail.com>
2023-04-17 23:42:16 -0300

    feat: card dinamic part 1 added

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -30,3 +30,73 @@ const btnDropdownSelect = document.querySelector(".js-open-select-custom");
 btnDropdownSelect.addEventListener("click", () => {
   btnDropdownSelect.parentElement.classList.toggle("active");
 });
+
+const areaPokemons = document.getElementById("js-list-pokemon");
+
+function createCardPokemon(code, type, nome, imagePok) {
+  let card = document.createElement("button");
+  card.classList = `card-pokemon js-open-details-pokemon ${type}`;
+  areaPokemons.appendChild(card);
+
+  let image = document.createElement("div");
+  image.classList = "image";
+  card.appendChild(image);
+
+  let imageSrc = document.createElement("img");
+  imageSrc.classList = "thumb-img";
+  imageSrc.setAttribute("src", imagePok);
+  image.appendChild(imageSrc);
+
+  let infoCardPokemon = document.createElement("div");
+  infoCardPokemon.classList = "info";
+  card.appendChild(infoCardPokemon);
+
+  let infoTextPokemon = document.createElement("div");
+  infoCardPokemon.classList = "text";
+  card.appendChild(infoTextPokemon);
+
+  let codePokemon = document.createElement("span");
+  codePokemon.textContent =
+    code < 10 ? `#00${code}` : code < 100 ? `#0${code}` : `#${code}`;
+  infoTextPokemon.appendChild(codePokemon);
+}
+
+function listingPokemons(urlApi) {
+  axios({
+    method: "GET",
+    url: urlApi,
+  }).then((response) => {
+    const countPokemons = document.getElementById("js-count-pokemons");
+
+    const { results, next, count } = response.data;
+
+    countPokemons.innerText = count;
+
+    results.forEach((pokemon) => {
+      let urlApiDetails = pokemon.url;
+
+      axios({
+        method: "GET",
+        url: `${urlApiDetails}`,
+      }).then((response) => {
+        const { name, id, sprites, types } = response.data;
+
+        const infoCard = {
+          nome: name,
+          code: id,
+          image: sprites.other.dream_world.front_default,
+          type: types[0].type.name,
+        };
+
+        createCardPokemon(
+          infoCard.code,
+          infoCard.type,
+          infoCard.nome,
+          infoCard.image
+        );
+      });
+    });
+  });
+}
+
+listingPokemons(" https://pokeapi.co/api/v2/pokemon?limit=9&offset=0");
